8e9f09703cd0dcd80967774ee884d760e4236433
Nacho Lorente <ignaciolorentegarcia@gmail.com>
2023-02-01 16:38:25 +0100

    Iteration 1, 2 WIP

--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,27 @@
+const { response } = require("express");
+const mongoose = require("mongoose");
+const Drone = require("../models/Drone.model");
+
+require("../db/index");
+
 // Iteration #1
+const drones = [
+  { name: "Creeper XL 500", propellers: 3, maxSpeed: 12 },
+  { name: "Racer 57", propellers: 4, maxSpeed: 20 },
+  { name: "Courier 3000i", propellers: 6, maxSpeed: 18 },
+];
+
+mongoose
+  .connect(MONGO_URI)
+  .then((x) => {
+    console.log(
+      `Connected to Mongo! Database name: "${x.connections[0].name}"`
+    );
+    Drone.create(drones).then((resp) => {
+      //mongoose.connection.close();
+      mongoose.disconnect();
+    });
+  })
+  .catch((err) => {
+    console.error("Error connecting to mongo: ", err);
+  });
